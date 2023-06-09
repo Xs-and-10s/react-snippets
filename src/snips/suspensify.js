79124b2bff4551c51284dd4d9ee32a5a13650efa
Mark Manley <mark.manley3@icloud.com>
2023-06-09 15:16:39 -0400

    snippet: suspensify promises like fetch to work with Suspense component

---
+++ src/snips/suspensify.js
@@ -0,0 +1,39 @@
+/* 
+! A function that wraps a promise with a 
+! Suspense-compatible intergace
+? The promise can be unwrapped by the 
+? return value's `read()` method.
+*/
+export const suspensify = (promise) => {
+  // ! type Status = 'pending' | 'error' | 'success';
+  let status = 'pending';
+  let response;
+
+  // ? Keep track of the promise status
+  const suspender = promise.then(
+    (res) => {
+      status = 'success';
+      response = res;
+    },
+    (err) => {
+      status = 'error';
+      response = err;
+    }
+  );
+
+  // ? `read()` sends the data to the proper renderer
+  return {
+    read() {
+      // ! (a) send to <Suspense>
+      if (status === 'pending') {
+        throw suspender;
+      }
+      // ! (b) send to <ErrorBoundary>
+      else if (status === 'error') {
+        throw response;
+      }
+      // ! (c) continue to render as usual with the data!
+      else return response;
+    },
+  };
+};
