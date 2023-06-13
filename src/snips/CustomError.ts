84657a9c8b8f9bcba3e9a9717d6b4564220a0265
Mark Manley <mark.manley3@icloud.com>
2023-06-12 21:47:24 -0400

    snippet<CustomError>: show example

--- src/snips/CustomError.ts
+++ src/snips/CustomError.ts
@@ -22,3 +22,11 @@ export class CustomError<T extends string> extends Error {
 type ExampleErrors = 'EASTER_BUNNY_DNE_ERROR' | 'TOOTH_FAIRY_DNE_ERROR';
 
 class ChildhoodBeliefsError extends CustomError<ExampleErrors> {}
+
+function ExampleTryCatch() {
+  try {
+    throw new CustomError<'DNE'>({ name: 'DNE', message: 'Does Not Exist' });
+  } catch (e) {
+    console.error(e);
+  }
+}
