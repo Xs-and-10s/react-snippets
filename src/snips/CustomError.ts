9c0da6708ce59b06502717d465e9919fbd309bd5
Mark Manley <mark.manley3@icloud.com>
2023-06-12 21:17:32 -0400

    snippet<CustomError>: create custom error types

---
+++ src/snips/CustomError.ts
@@ -0,0 +1,24 @@
+export class CustomError<T extends string> extends Error {
+  name: T;
+  message: string;
+  cause: any;
+
+  constructor({
+    name,
+    message,
+    cause,
+  }: {
+    name: T;
+    message: string;
+    cause?: any;
+  }) {
+    super();
+    this.name = name;
+    this.message = message;
+    this.cause = cause;
+  }
+}
+
+type ExampleErrors = 'EASTER_BUNNY_DNE_ERROR' | 'TOOTH_FAIRY_DNE_ERROR';
+
+class ChildhoodBeliefsError extends CustomError<ExampleErrors> {}
